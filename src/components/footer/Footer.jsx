554112069f81e778eb33c3a37bd1e9ccc18fac24
Deep Bansode <drasticoder@gmail.com>
2023-12-05 01:17:18 +0530

    Add dependencies and update styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="column">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+        </div>
+        <div className="column">
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+          
+        </div>
+        <div className="column">
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+          
+        </div>
+        <div className="column">
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Link</a>
+          
+        </div>
+      </div>
+      <div className="bottom-footer">
+        <div>COpywrite 2024 Alegria</div>
+        <div>Socials</div>
+        <div>Powered By Alegria</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
