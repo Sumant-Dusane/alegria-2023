de6421ef52bd043f29a9693eb24ff6bae477bdba
Sumant-Dusane <sumant.dusane1966@gmail.com>
2023-12-05 02:59:56 +0530

    initial gsap setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,26 +3,50 @@ import "./page.scss";
 import { Planets } from "@/utils/planets";
 import ArrowBtn from "@/assets/images/arrow.svg";
 
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { Timeline, Tween } from "react-gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Home() {
   return (
     <div className="landing">
-      <div className="planets">
-        <div className="planet" >
-          <Image src={Planets.Planet1} alt="Planet 1" />
+      <ScrollTrigger
+        trigger="trigger"
+        start="top center"
+        end="100vh center"
+        scrub={0.5}
+        marker={true}
+        pin={true}
+      >
+        <div className="planets">
+          <Timeline>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet1} alt="Planet 1" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet2} alt="Planet 2" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet3} alt="Planet 3" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet4} alt="Planet 4" />
+              </div>
+            </Tween>
+          </Timeline>
         </div>
-        <div className="planet">
-          <Image src={Planets.Planet2} alt="Planet 2" />
-        </div>
-        <div className="planet">
-          <Image src={Planets.Planet3} alt="Planet 3" />
-        </div>
-        <div className="planet">
-          <Image src={Planets.Planet4} alt="Planet 4" />
-        </div>
-      </div>
-      <button className="navigate-btn">
-        <Image src={ArrowBtn} />
-      </button>
+        <button className="navigate-btn">
+          <Image src={ArrowBtn} />
+        </button>
+      </ScrollTrigger>
     </div>
   )
 }
