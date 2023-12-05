6840558465cbbf82be0ec765c87c51594338d740
Deep Bansode <drasticoder@gmail.com>
2023-12-05 20:08:13 +0530

    code clean (#3)
    * code clean
    
    * initial gsap setup
    
    ---------
    
    Co-authored-by: Sumant-Dusane <sumant.dusane1966@gmail.com>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,15 @@
+"use client"
+
 import Navbar from '@/components/Navbar/Navbar';
 import '../assets/style/style.scss';
+import { useEffect, useState } from 'react';
 
 export default function RootLayout({ children }) {
+  const [currentPlanet, setCurrentPlanet] = useState('planet1');
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <Navbar currentPlanet={currentPlanet} setCurrentPlanet={setCurrentPlanet} />
         {children}
         </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,30 +2,51 @@ import Image from "next/image";
 import "./page.scss";
 import { Planets } from "@/utils/planets";
 import ArrowBtn from "@/assets/images/arrow.svg";
-import Link from "next/link";
+
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { Timeline, Tween } from "react-gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
     <div className="landing">
-      <div className="planets">
-        <div className="planet">
-          {/* <Link href="/universe/[id]" as="/universe/marvel"> */}
-            <Image src={Planets.Planet1} alt="Planet 1" />
-          {/* </Link> */}
+      <ScrollTrigger
+        trigger="trigger"
+        start="top center"
+        end="100vh center"
+        scrub={0.5}
+        marker={true}
+        pin={true}
+      >
+        <div className="planets">
+          <Timeline>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet1} alt="Planet 1" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet2} alt="Planet 2" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet3} alt="Planet 3" />
+              </div>
+            </Tween>
+            <Tween>
+              <div className="planet">
+                <Image src={Planets.Planet4} alt="Planet 4" />
+              </div>
+            </Tween>
+          </Timeline>
         </div>
-        <div className="planet">
-          <Image src={Planets.Planet2} alt="Planet 2" />
-        </div>
-        <div className="planet">
-          <Image src={Planets.Planet3} alt="Planet 3" />
-        </div>
-        <div className="planet">
-          <Image src={Planets.Planet4} alt="Planet 4" />
-        </div>
-      </div>
-      <button className="navigate-btn">
-        <Image src={ArrowBtn} />
-      </button>
+        <button className="navigate-btn">
+          <Image src={ArrowBtn} />
+        </button>
+      </ScrollTrigger>
     </div>
   )
 }
