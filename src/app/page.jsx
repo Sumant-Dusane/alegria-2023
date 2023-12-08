dd93f9ee1408f347aa691df3b954b6f5e80f81a9
Sumant-Testbook <sumant.dusane@testbook.com>
2023-12-08 20:15:29 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,51 +1,21 @@
 "use client"
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Image from "next/image";
 import "./page.scss";
-import { Planets } from "@/utils/planets";
-import ArrowBtn from "@/assets/images/arrow.svg";
+import Image from 'next/image';
+
+import planet1 from "../assets/textures/planet1.png";
+import planet2 from "../assets/textures/planet2.png";
+import planet3 from "../assets/textures/planet3.png";
+import planet4 from "../assets/textures/planet4.png";
 
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
-  const planetDivRef = useRef(null);
-  const planetsRef = {
-    planet1: useRef(null),
-    planet2: useRef(null),
-    planet3: useRef(null),
-    planet4: useRef(null)
-  }
-
-  // useEffect(() => {
-  //   const planets = [planetsRef.planet1.current, planetsRef.planet2.current, planetsRef.planet3.current, planetsRef.planet4.current];
-  //   const landingTop = planetDivRef.current?.offsetTop;
-  //   const landingBottom = planetDivRef.current?.offsetHeight + landingTop;
-
-  //   const planetScrollTimeline = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: planetDivRef.current,
-  //       start: 'top top',
-  //       end: 'center top+=10',
-  //       scrub: true,
-  //       markers: true
-  //     }
-  //   });
-  //   planets.forEach((planet, index) => {
-  //     planetScrollTimeline.to(
-  //       planet, {
-  //         x: index % 2 === 0 ? '-100%' : '100%',
-  //         scale: 1.5
-  //       }
-  //     )
-  //   })
-  // }, []);
 
   useEffect(() => {
-  // let scrollPos = 0;
-
   return () => {
     gsap.to('.landing', {
       scrollTrigger: {
@@ -55,71 +25,33 @@ export default function Home() {
         markers: true,
         pin: true,
         scrub: true,
-        // onUpdate: (self) => {
-        //   console.log('this is scroll postiion:', self.scroll());
-        //   scrollPos = self.scroll();
-        // },
       },
     })
-
-
-    // const boxes = gsap.utils.toArray('.planet');
-    const planets = [planetsRef.planet1.current, planetsRef.planet2.current, planetsRef.planet3.current, planetsRef.planet4.current];
-    
-    let l = Math.sqrt(Math.pow((planetDivRef.current.clientHeight/2), 2) + Math.pow((planetDivRef.current.clientWidth/2), 2));
-    console.log(l);
-
-    planets.forEach((planet, index) => {
-      index += 1;
-      let isOdd = index % 2 !== 0;
-      const aa = {
-        scale: 1/(index),
-        x: Math.abs(index-4)*100,
-        y: Math.abs(index)*-100
-      }
-      // gsap.to(box, {
-      //   scale: 1.5,
-      //   x: i % 2 == 0 ? `-300%` : `300%`,
-
-      //   scrollTrigger: {
-      //     // x: i%2 == 0? -(300 *i) : (300*i),
-      //     trigger: box,
-      //     scrub: true,
-      //     markers: true
-      //   },
-      // });
-      gsap.fromTo(planet, 
-        aa, aa
-      );
-    });
   }
   }, []);
 
   return (
     <>
-      <div className="landing" ref={planetDivRef}>
-        <div className="landing__upper">
-
+      <div className="landing">
+        <div className="universe star">
+          <figure>
+            <Image src={planet1} />
+          </figure>
         </div>
-        <div className="landing__lower">
-          <div className="landing__lower__planets">
-            <div className="planet" ref={planetsRef.planet1}>
-              <Image draggable={false} src={Planets.Planet1} alt="Planet 1" />
-            </div>
-            <div className="planet" ref={planetsRef.planet2}>
-              <Image draggable={false} src={Planets.Planet2} alt="Planet 2" />
-            </div>
-            <div className="planet" ref={planetsRef.planet3}>
-              <Image draggable={false} src={Planets.Planet3} alt="Planet 3" />
-            </div>
-            <div className="planet" ref={planetsRef.planet4}>
-              <Image draggable={false} src={Planets.Planet4} alt="Planet 4" />
-            </div>
-          </div>
-
-          <button className="landing__lower__navigate-btn">
-            <Image src={ArrowBtn} />
-          </button>
+        <div className="universe ">
+        <figure>
+            <Image src={planet2} />
+          </figure>
+        </div>
+        <div className="universe ">
+        <figure>
+            <Image src={planet3} />
+          </figure>
+        </div>
+        <div className="universe ">
+        <figure>
+            <Image src={planet4} />
+          </figure>
         </div>
       </div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/assets/images/logo.svg";
-import { Planets } from "@/utils/planets";
 import "./navbar.scss";
 import { useState } from "react";
 
 const Navbar = ({ currentPlanet, setCurrentPlanet }) => {
     const [planetSelectorVisibility, setPlanetSelectorVisibility] = useState(false);
-    const mapPlanets = () => {
-        return Planets.map((planet, index) => {
-            return <li><button onClick={() => setCurrentPlanet('planet' + index + 1)}><Image src={planet} alt={'planet' + index + 1} /> Planet {index + 1}</button></li>
-        })
-    }
 
     return (
         <nav className="navbar">
@@ -27,30 +21,7 @@ const Navbar = ({ currentPlanet, setCurrentPlanet }) => {
                 <li><Link href="#" >Brochure</Link></li>
                 <li><Link href="#" >More</Link></li>
             </ul>
-
-            <div className="navbar__planet-selector">
-                <button onClick={() => setPlanetSelectorVisibility(true)}>
-                    <Image
-                        src={
-                            currentPlanet === 'planet1' ?
-                                Planets.Planet1 :
-                                currentPlanet === 'planet2' ?
-                                    Planets.Planet2 :
-                                    currentPlanet == 'planet3' ?
-                                        Planets.Planet3 :
-                                        Planets.Planet4
-                        }
-                        alt={currentPlanet}
-                    />
-                </button>
-                <ul className={planetSelectorVisibility ? "navbar__planet-selector__content show" : "navbar__planet-selector__content"}>
-                    {/* {mapPlanets()} */}
-                    <li><button onClick={() => {setCurrentPlanet('planet1'); setPlanetSelectorVisibility(false);}}><Image src={Planets.Planet1} alt={currentPlanet} /> Planet 1</button></li>
-                    <li><button onClick={() => {setCurrentPlanet('planet2'); setPlanetSelectorVisibility(false);}}><Image src={Planets.Planet2} alt={currentPlanet} /> Planet 2</button></li>
-                    <li><button onClick={() => {setCurrentPlanet('planet3'); setPlanetSelectorVisibility(false);}}><Image src={Planets.Planet3} alt={currentPlanet} /> Planet 3</button></li>
-                    <li><button onClick={() => {setCurrentPlanet('planet4'); setPlanetSelectorVisibility(false);}}><Image src={Planets.Planet4} alt={currentPlanet} /> Planet 4</button></li>
-                </ul>
-            </div>
+            <div></div>
         </nav>
     )
 }
